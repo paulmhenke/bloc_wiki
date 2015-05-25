d9f93970740ec67cfc2c7193dd365687a6e8eb3b
paulhenke <paulmhenke@gmail.com>
2015-05-25 17:46:58 +0000

    fixed email confirmation link

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {:host => "https://wiki-paulhenke.c9.io"}
+  config.action_mailer.default_url_options = {host: "https://wiki-paulhenke.c9.io"}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
