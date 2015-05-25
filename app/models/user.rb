432042a6cf0f7adedee22ac6c97efc5bb5a4b5d5
paulhenke <paulmhenke@gmail.com>
2015-05-25 13:51:44 +0000

    added minimal email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
     has_secure_password
     
-    validates_uniqueness_of :email
-    validates :password, length: { minimum: 8 }
+  validates_uniqueness_of :email 
+  validates_format_of :email, :with => /@/
+  validates :password, length: { minimum: 8 }
+    
 end
+     
+  
