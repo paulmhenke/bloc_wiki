c40eac8135ff2d02864a0ae6cedfa262b1c059f9
paulhenke <paulmhenke@gmail.com>
2015-05-30 13:35:20 +0000

    Starting working on views/layouts, met with Levi

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,15 @@ class SessionsController < ApplicationController
   def create
       user = User.find_by_email(params[:email].downcase)
       if user && user.authenticate(params[:password])
-      if user.email_confirmed
-          sign_in user
-        redirect_back_or user
-      else
-        flash.now[:error] = 'Please activate your account by following the 
-        instructions in the account confirmation email you received to proceed'
-        render 'new'
-      end
+        if user.email_confirmed
+          session[:user_id] = user.id
+          flash[:notice] = "You have logged in!"
+          redirect_to root_url
+        else
+          flash[:notice] = 'Please activate your account by following the 
+          instructions in the account confirmation email you received to proceed'
+          render 'new'
+        end
       else
         flash.now[:error] = 'Invalid email/password combination' 
         render 'new'
