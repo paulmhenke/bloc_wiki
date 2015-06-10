d9e9e10a8063f4a127b42713c57d146febbe6888
paulhenke <paulmhenke@gmail.com>
2015-06-10 19:04:04 +0000

    seeds checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,20 @@
 require 'faker'
 
 member = User.create!(name: "Member User", email: "member@example.com", password: "helloworld", email_confirmed: true)
-member.save!   
+member.save!  
+
+premium = User.create!(name: "Premium User", email: "premium@example.com", password: "helloworld", email_confirmed: true, role: premium)
+premium.save!
+
+admin = User.create!(name: "Admin", email: "admin@example.com", password: "helloworld", email_confirmed: true, role: admin)
+admin.save!
     
 10.times do
-Wiki.create!(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph, user_id: member.id)
+  Wiki.create!(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph, user_id: member.id)
+end
+
+5.times do
+  Wiki.create!(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph, user_id: premium.id)
 end
 
 puts "Seed Finished"
