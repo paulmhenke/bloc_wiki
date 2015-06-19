bfa0cf3ae58e4ba3d55c1a45d2cb74950d8644cd
paulhenke <paulmhenke@gmail.com>
2015-06-19 13:35:45 +0000

    fixed scope with Levi

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,6 +1,21 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
   
-  scope :visible_to, -> (user) { user && user.standard? ? where(private: false) : where(private: false) && where(user_id: user.id) }
-  #scope :visible_to, -> (user) { user  && user.premium? ? all : where(private: false) }
+  #scope :visible_to, -> (user) { user && user.standard? ? where(private: false) : where(private: false) && where(user_id: user.id) }
+  # return wikis that are public OR wikis that are private and belong to the user
+  scope :visible_to_premium, -> (user) { where("private = ? OR (user_id = ? AND private = ?)", false, user.id, true) }
+  scope :visible_to_standard, -> { where(private: false) }
+  
+  def self.visible_to(user)
+    case user.role
+    when "admin"
+      all
+    when "premium"
+      visible_to_premium(user)
+    when "standard"
+      visible_to_standard
+    end
+  end
+  
+  
 end
